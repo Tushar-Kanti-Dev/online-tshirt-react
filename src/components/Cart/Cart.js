80536d880b40dce23e530d324388a595664c2d02
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-01-30 13:09:36 +0600

    conditional rendering options added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart,handelRemoveFromCart}) => {
+    let command;
+    if(cart.length===0){
+        command = <p>Added atlest one item</p>
+    }else if(cart.length === 1){
+        command = <p>select more</p>
+    }
     return (
         <div>
             <h2>Order Summery</h2>
             <h4>Selected Items: {cart.length}</h4>
+            {command}
             {
                 cart.map(tShirt=> <p>
                     
@@ -13,6 +20,9 @@ const Cart = ({cart,handelRemoveFromCart}) => {
                     <button onClick={()=> handelRemoveFromCart(tShirt)}>X</button>
                 </p>)
             }
+            {/* ternary operator  */}
+            {cart.length !==4 ? <p>keep adding</p>: <button>Remove All</button>}
+            {cart.length !==0 && <button className='orange'>review items</button>}
         </div>
     );
 };
